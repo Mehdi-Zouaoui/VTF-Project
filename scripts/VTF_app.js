76a5a79f8f6415b9a5a356c60326f738574908c5
Mehdi <md1.zouaoui@gmail.com>
2019-03-18 13:40:32 +0100

    Second commit

--- scripts/VTF_app.js
+++ scripts/VTF_app.js
@@ -1,3 +1,5 @@
+
+
 const game = new Phaser.Game(
     window.innerWidth,
     window.innerHeight,
@@ -13,7 +15,7 @@ const game = new Phaser.Game(
 
 let ball;
 let pad;
-let bricksGroup;
+let PNJGroup;
 let timerText, timer = 0;
 
 function preload() {
@@ -21,23 +23,31 @@ function preload() {
     // game.scale.pageAlignHorizontally = true;
     // game.scale.pageAlignVertically = true;
     // game.stage.backgroundColor = '#eee';
+    game.load.image('ball', 'asset/img/ball.png');
+    game.load.image('pnj_1', 'asset/img/pnj_giroud.png', 100 ,33);
+    game.load.image('pnj_2', 'asset/img/pnj_kante.png');
+    game.load.image('pnj_3', 'asset/img/pnj_koscieny.png');
+    
 };
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    
+ 
+
     // Création du groupe de briques
-    bricksGroup = game.add.physicsGroup();
+    PNJGroup = game.add.physicsGroup();
 
     // Génération des briques
-    for (let y = 0; y < 7; y++) {
-        for (let x = 0; x < 10; x++) {
-            let sprite = new Phaser.Sprite(game, x * 100 + (game.world.width - 1000) / 2, y * 33, 'brick_' + (x % 10 + 1));
+    for (let y = 0; y < 1; y++) {
+        for (let x = 0; x < 3; x++) {
+            let sprite = new Phaser.Sprite(game, x * 100 + (game.world.width - 1000) / 2, y * Math.floor(Math.random() * (game.world.height - 100)) + 10 ,'pnj_' + (x % 10 + 1));
             // sprite.scale.setTo(0.2604, 0.2604); 
-            bricksGroup.add(sprite);
+            PNJGroup.add(sprite);
         }
     }
-    bricksGroup.forEach(item => {
+    PNJGroup.forEach(item => {
         item.body.immovable = true;
     });
 
@@ -49,15 +59,13 @@ function create() {
     ball.body.velocity.set(300, -300);
     ball.body.bounce.set(1);
 
-    ball_2 = game.add.sprite(game.world.width / 2, game.world.height , 'ball');
-    ball_2.anchor.set(0.5);
-    game.physics.arcade.enable(ball_2);
-    ball_2.body.collideWorldBounds = true;
-    ball_2.body.velocity.set(300, -300);
-    ball_2.body.bounce.set(1.05);
+    
+
+    
     
     // Création du pad
-    pad = game.add.sprite(game.world.width / 2, game.world.height - 5, 'pad');
+    pad = game.add.sprite(game.world.width / 2, game.world.height - 5, 'ball');
+    pad.scale.setTo(0.5,0.5);
     pad.anchor.set(0.5, 1);
     game.physics.arcade.enable(pad);
     pad.body.immovable = true;
@@ -66,8 +74,8 @@ function create() {
     game.physics.arcade.checkCollision.down = false;
     ball.checkWorldBounds = true;
     ball.events.onOutOfBounds.add(function(){
-        window.alert('Game over!');
-        window.location.reload();
+      //  window.alert('Game over!');
+       // window.location.reload();
     }, this);
 
     // Timing
@@ -76,16 +84,37 @@ function create() {
 }
 
 function update() {
-    game.physics.arcade.collide(ball, bricksGroup, ballHitBrick);
-    game.physics.arcade.collide(ball_2, bricksGroup, ballHitBrick);
-    game.physics.arcade.collide(ball, pad, ballHitPad);
-    game.physics.arcade.collide(ball_2, pad, ballHitPad);
-
     
-    if (LEAP.connected === true) pad.x  =  LEAP.position.x ;
-    else pad.x = game.input.x;
+    let graphics = null;
+
+  //  game.physics.arcade.collide(ball, PNJGroup, ballHitBrick);
+    
+    game.physics.arcade.collide(ball, pad, ballHitPad);
+    
+    if (LEAP.connected === true){
+
+    pad.x  =  LEAP.position.x ;
+    pad.y  = LEAP.position.y ; 
+    pad.radius = LEAP.position.radius;
+   
+    
+    
+    graphics = game.add.graphics();
+   
+   // graphics.clear(); 
+    
+}
+
+   
+        
+
+   // else pad.x = game.input.x;
     timerText.setText('Temps: ' + (timer / 1000).toFixed(1) + 's');
 
+  
+   
+
+    
    
 }
 
@@ -96,7 +125,7 @@ function render() {
 function ballHitBrick(ball, brick) {
     brick.kill();
 
-    const remainingBalls = bricksGroup.children.reduce((total, brick) => brick.alive ? total + 1 : total, 0);
+    const remainingBalls = PNJGroup.children.reduce((total, brick) => brick.alive ? total + 1 : total, 0);
     if (remainingBalls === 0) {
         window.alert('Bravo!\nVous avez complété le jeu en '+ (timer / 1000).toFixed(1) +' secondes !');
         window.location.reload();
@@ -107,15 +136,3 @@ function ballHitPad(ball, pad) {
     ball.body.velocity.x = -1 * 5 * (pad.x - ball.x);
 }
 
-function renderCircle(frame, gesture) {
-    const centerPosition = get2dCoords(gesture.center, frame, canvas);
-    const radius         = gesture.radius;
-
-    context.strokeStyle  = 'pink';
-    context.lineWidth    = 3;
-    context.beginPath();
-    context.arc(centerPosition.x, centerPosition.y, radius, 0, Math.PI * 2);
-    context.stroke();
-    context.closePath();
-
-}
--- scripts/leapinput.js
+++ scripts/leapinput.js
@@ -1,12 +1,16 @@
 const LEAP = {
     position : {
         x : 0,
-        y : 0
+        y : 0,
+        centerPosition : 0,
+        radius : 0,
                 },
     connected : false,
+    gesture_done : false,
+    
 };
 
-const controller = new Leap.Controller();
+const controller = new Leap.Controller({enableGestures: true}   );
 controller.connect();
 
 controller.on('deviceStreaming', function(){
@@ -16,13 +20,9 @@ controller.on('deviceStreaming', function(){
 
 controller.on('deviceStreaming', function(){
     LEAP.connected = true;
-    console.log(' ❌Leap Motion is disconnected ');
+    console.log(' ❌ Leap Motion is disconnected ');
 });
-controller.on('CircleGesture' , function(){
-    if(gesture.type = "circle"){
-        console.log('circle');
-    }
-})
+  
 controller.on('frame' , function(frame){
     let hand = frame.hands[0];
     if(!hand)return;
@@ -30,7 +30,36 @@ controller.on('frame' , function(frame){
     const palm = get2dCoords(hand.stabilizedPalmPosition , frame);
     LEAP.position.x = palm.x;
     LEAP.position.y = palm.y;
+
+
+    frame.gestures.forEach(function(gesture) {
+      
+        switch (gesture.type){
+            case "circle":
+                let  circleProgress = gesture.progress;
+                let completeCircles = Math.floor(circleProgress);
+                console.log(completeCircles);
+                let circle = renderCircle(gesture);
+                LEAP.position.radius = circle.radius;
+                LEAP.gesture_done = true;
+                break;
+            case "keyTap":
+                console.log("Key Tap Gesture");
+                
+                break;
+            case "screenTap":
+                console.log("Screen Tap Gesture");
+                break;
+            case "swipe":
+                console.log("Swipe Gesture");
+                break;
+        
+          
+    }
+
+    });
 });
+
 
 function get2dCoords(leapPosition, frame) {
     const interactionBox  = frame.interactionBox;
@@ -40,4 +69,14 @@ function get2dCoords(leapPosition, frame) {
         x : normalizedPoint[0] * window.innerWidth,
         y : (1 - normalizedPoint[1]) * window.innerHeight,
     }
+}
+function renderCircle(gesture ) {
+    const radius = gesture.radius;
+   
+    console.log("Circle");
+
+    return {
+        radius : radius,
+     
+}
 }
